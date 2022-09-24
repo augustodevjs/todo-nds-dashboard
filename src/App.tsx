111756e372fb013f30502a23b5a588923a7bfd74
augusto <augustosaaude@gmail.com>
2022-09-23 22:11:56 -0300

    chore: remove dependencies unnecessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import { Router } from './routes/Router';
-import { GlobalStyle } from './styles/global';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 export function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Router />
-    </>
-  );
+  return <h1>askdasdkjaskdjaksjk12k3klasdakl</h1>;
 }
--- src/pages/dashboard/Lists/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Sidebar } from '../components/Sidebar';
-
-export function Lists() {
-  return <Sidebar />;
-}
